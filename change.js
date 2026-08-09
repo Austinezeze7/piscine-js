ba5c55522ebfb1de7468dcf3ee373c1b55d06591
amuyonga <austinezeze1@gmail.com>
2026-08-09 14:54:05 +0300

    modifying properties of js

---
+++ change.js
@@ -0,0 +1,19 @@
+// change.js
+
+// Define the sourceObject
+const sourceObject = {
+  num: 42,
+  bool: true,
+  str: "some text",
+  log: console.log,
+};
+
+function get(key) {
+  return sourceObject[key];
+}
+
+function set(key, value) {
+  sourceObject[key] = value;
+  return value;
+}
+
